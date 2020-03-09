7cdc5e5811577ca67a78faac039ecdea59b22af3
Per Mortensen <per@permortensen.com>
2020-03-09 18:48:16 +0100

    Initial commit

---
+++ whatever.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+set -o nounset
+prefix="$"
+executed=0
+
+# git
+if git rev-parse --is-inside-work-tree 1> /dev/null 2>&1 ; then
+  echo "$prefix git fetch --all --prune --tags"
+  git fetch --all --prune --tags
+
+  # if current branch has an upstream
+  if git rev-parse --abbrev-ref '@{u}' 1> /dev/null 2>&1; then
+    if git diff-index --quiet HEAD --; then
+      echo "$prefix git pull"
+      git pull
+    else
+      echo "Dirty work dir, won't pull"
+    fi
+  fi
+
+  executed=1
+fi
+
+# npm/yarn
+if [ -f "package.json" ]; then
+  if [ -f "yarn.lock" ]; then
+    echo "$prefix yarn install"
+    yarn install
+  else
+    echo "$prefix npm install"
+    npm install
+  fi
+
+  executed=1
+fi
+
+# bower
+if [ -f "bower.json" ]; then
+  echo "$prefix bower install"
+  bower install
+  executed=1
+fi
+
+# python
+if [ -f "Pipfile" ]; then
+  echo "$prefix pipenv install --dev"
+  pipenv install --dev
+  executed=1
+elif [ -f "requirements.txt" ]; then
+  echo "$prefix pip3 install -r requirements.txt"
+  pip3 install -r requirements.txt
+  executed=1
+fi
+
+# ruby
+if [ -f "Gemfile" ]; then
+  echo "$prefix bundle install"
+  bundle install
+  executed=1
+fi
+
+# msbuild
+if ls ./*.sln 1> /dev/null 2>&1 || ls ./*.csproj 1> /dev/null 2>&1; then
+  echo "$prefix dotnet restore"
+  dotnet restore
+  executed=1
+fi
+
+# gradle
+if [ -f "gradlew" ]; then
+  echo "$prefix ./gradlew"
+  ./gradlew
+  executed=1
+fi
+
+if (( executed == 0 )); then
+  echo "Found nothing to run."
+  exit 1
+fi
